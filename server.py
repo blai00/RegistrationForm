df62e95ce3e9afbb5f8f68bb1d1bd9ea0afde4fa
blai00 <brandon.lai00@gmail.com>
2016-11-13 11:30:12 -0800

    validation works

--- server.py
+++ server.py
@@ -12,18 +12,34 @@ def index():
 
 @app.route("/process", methods=['POST'])
 def validation():
-	if request.form == {}:
-		flash('All fields are blank.')
+	
 	data = request.form
 	print data
-
-	if len(request.form['email'] or request.form['first_name'] or request.form['last_name'] or request.form['password'] or request.form['confirm_password']) < 1:
-		flash('One or more field is blank. Please make sure all fields are filled out.')
+	if not request.form['first_name']:
+		flash('Plese enter a name')
 		return redirect('/')
-	if len(request.form['password']) < 8:
+	elif len(request.form['first_name']) < 2 :
+		flash('Your first name needs to be longer than 2 characters.')
+		return redirect('/')
+	if not request.form['email']:
+		flash('Please enter an email address.')
+		return redirect('/')
+	elif len(request.form['email']) < 2:
+		flash('Your email has to be longer than 2 characters')
+		return redirect('/')
+	if not request.form['last_name']:
+		flash('Plese enter a last name')
+		return redirect('/')
+	elif len(request.form['last_name']) < 1 :
+		flash('Your last name needs to be longer than 1 character.')
+		return redirect('/')
+	if not request.form['password']:
+		flash('Please enter a password.')
+		return redirect('/')
+	elif len(request.form['password']) < 8:
 		flash("Password needs to be 8 characters long.")
 		return redirect('/')
-	if not (NAME_REGEX.match(request.form['name']) or NAME_REGEX.match(request.form['last_name'])):
+	if not (NAME_REGEX.match(request.form['first_name']) or NAME_REGEX.match(request.form['last_name'])):
 		flash("Invalid Name or Last Name. Please enter a valid name")
 		return redirect('/')
 	if request.form['password'] != request.form['confirm_password']:
